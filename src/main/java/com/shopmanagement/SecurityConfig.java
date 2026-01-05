655005d5c92ca743fafdebfbfb85b4d057a90b97
akhila595 <akhilaradharapu5@gmail.com>
2026-01-05 11:42:55 +0530

    cors chnages

--- src/main/java/com/shopmanagement/SecurityConfig.java
+++ src/main/java/com/shopmanagement/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .cors(cors -> {})                 // Spring Boot 3.x style
+        		.cors(org.springframework.security.config.Customizer.withDefaults())               
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sm ->
                         sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
